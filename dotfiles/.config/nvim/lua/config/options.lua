fc7e720f0ea8c823b8b90e38f103a6bcb431bb4b
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2025-11-29 08:30:15 +1100

    nvim: enable copy to clipboard over SSH

--- dotfiles/.config/nvim/lua/config/options.lua
+++ dotfiles/.config/nvim/lua/config/options.lua
@@ -2,3 +2,27 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 vim.opt.relativenumber = false
+
+-- Clipboard, vim pasting from ssh guest doesn't work, use CTRL+V
+-- copied from https://github.com/LazyVim/LazyVim/discussions/4602#discussioncomment-10995590
+-- yanky must be disabled to avoid errors
+if vim.env.SSH_TTY then
+  vim.opt.clipboard:append("unnamedplus")
+  local function paste(_)
+    return function()
+      local content = vim.fn.getreg('"')
+      return vim.split(content, "\n")
+    end
+  end
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      ["+"] = paste("+"),
+      ["*"] = paste("*"),
+    },
+  }
+end
