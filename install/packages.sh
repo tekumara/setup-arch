9caedf9476ab93c8f0ec4a92af27916c6048899c
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2026-01-04 22:45:35 +1100

    packages: upgrade -git packages when run

--- install/packages.sh
+++ install/packages.sh
@@ -10,12 +10,18 @@ omarchy-update
 packages=(
   chromium-widevine
   helium-browser-bin
-  quickemu-git
   rsync
   stow
   uv
   zen-browser-bin
+)
+
+packages_git=(
+  quickemu-git
   zmx-git
 )
 
 yay -S --answerclean None --answerdiff None --needed --noconfirm "${packages[@]}"
+
+# install even if uptodate in order to fetch latest commits
+yay -S --answerclean None --answerdiff None --noconfirm "${packages_git[@]}"
