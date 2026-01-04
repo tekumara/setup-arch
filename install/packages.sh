d1c064f0017b8953f775cf85c17786b1faa64608
Oliver Mannion <125105+tekumara@users.noreply.github.com>
2026-01-04 22:34:06 +1100

    packages: refactor into single file

--- install/packages.sh
+++ install/packages.sh
@@ -5,9 +5,16 @@ set -euo pipefail
 export PATH=$PATH:$HOME/.local/share/omarchy/bin/
 export OMARCHY_PATH=$HOME/.local/share/omarchy
 
-script_dir="$(dirname "$(readlink -f "${BASH_SOURCE[0]:-$0}")")"
-
 omarchy-update
 
-sudo pacman -S --needed --noconfirm - < "$script_dir"/packages.pacman
-yay -S --answerclean None --answerdiff None --needed --noconfirm - < "$script_dir"/packages.yay
+packages=(
+  chromium-widevine
+  helium-browser-bin
+  rsync
+  stow
+  uv
+  zen-browser-bin
+  zmx-git
+)
+
+yay -S --answerclean None --answerdiff None --needed --noconfirm "${packages[@]}"
